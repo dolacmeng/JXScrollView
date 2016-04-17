13a50c1508cb240da41870f9dd5cfd34bd8061bc
xuweijie_18696781905 <151611438@qq.com>
2016-04-17 21:44:17 +0800

    Initial Commit

---
+++ JXScrollView/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  JXScrollView
+//
+//  Created by JackXu on 16/4/17.
+//  Copyright © 2016年 BFMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ JXScrollView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  JXScrollView
+//
+//  Created by JackXu on 16/4/17.
+//  Copyright © 2016年 BFMobile. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
